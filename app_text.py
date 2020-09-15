0ed45e43ce251bc82719d75163eadb785d6f52e0
Rust16Tk <Rust16Tk@yandex.ru>
2020-09-15 20:45:54 +0300

    что пошлон не так, надо думать

--- app_text.py
+++ app_text.py
@@ -1,9 +1,14 @@
 #проба чтения текста с файла с расширением txt
 import eel
 
-eel.init("web")
-eel.start("main.html", size=(700, 700))
 
-class read_texts():
-    def text_open(self):
-        pass
+@eel.expose
+def text_open(self):
+    f = open('', 'r')
+    
+    
+    
+
+
+eel.init("web")
+eel.start("main.html", size=(500, 700))
